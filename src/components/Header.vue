3db1e6fe41a3d842e80a4ddabc1c84b703b98722
Floortje <florian.tjeertes@ziggo.nl>
2025-03-21 00:03:13 +0100

    refactor: remove unused components and update layout structure

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand"> FrillyBits Github Project overview</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">project 1</li>
+          <li class="nav-item">project 2</li>
+          <li class="nav-item">project 3</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/Header.vue'
 </script>
 
 <template>
+  <Header />
   <main>
-    <TheWelcome />
+    <section class="about">
+      <h3>about</h3>
+      <p>about</p>
+    </section>
+    <section class="projects">
+      <section class="card">
+        <div class="card-header">
+          <h5>project title</h5>
+        </div>
+        <img src="https://picsum.photos/150" class="card-img-top" />
+        <section class="card">
+          <section class="card-body">
+            <p class="card-text">about project</p>
+            <a href="" class="btn btn-primary">go to project</a>
+          </section>
+        </section>
+      </section>
+    </section>
   </main>
 </template>
