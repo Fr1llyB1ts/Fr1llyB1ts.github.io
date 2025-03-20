5a83fe61a8b3486dad02c533701b754d30f59dcb
Floortje <florian.tjeertes@ziggo.nl>
2025-03-21 00:08:17 +0100

    feat: add Indevelopment view and redirect from home

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Indevelopment from '../views/Indevelopment.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView,
+    // },
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/indevelopment',
+    },
+    {
+      path: '/indevelopment',
+      name: 'indevelopment',
+      component: Indevelopment,
     },
   ],
 })
---
+++ src/views/Indevelopment.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <h1>page is under construction!</h1>
+  <p>please come back later</p>
+</template>
